776acdaa0e59d56ff0ba44cd2259f95fb14416af
lukaszkuchta <lukaszkuchta10@gmail.com>
2022-11-27 15:56:35 +0100

    drugi commit

--- src/main/java/Produkt.java
+++ src/main/java/Produkt.java
@@ -1,2 +1,20 @@
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Produkt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nazwa;
+    @Enumerated(value = EnumType.STRING)
+    private Kategoria kategoria;
+
+
+
 }
